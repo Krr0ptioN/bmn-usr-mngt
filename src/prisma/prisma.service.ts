25337826ef285e5ad84e17a4d10897de588dfd93
Krr0ptioN <mardin-pop@protonmail.com>
2023-05-25 21:21:43 +0300

    fix(prisma): wrong address

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+require("dotenv").config();
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
